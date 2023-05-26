def5bf3dd458e524527a617bc20a416c7fc56d7a
Masrafi404 <mdmasrafi902@gmail.com>
2023-05-26 19:45:50 +0600

    menu bannar

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,10 +5,14 @@ import ChefService from '../ChefService/ChefService';
 import PopularMenu from '../PopularMenu/PopularMenu';
 import Featured from '../Fearured/Featured';
 import TestiMonials from '../TestiMonials/TestiMonials';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <ChefService></ChefService>
